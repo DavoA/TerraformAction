b035a4cd2ecf9eed7fcd22a551d525a723a87fce
Your Name <you@example.com>
2024-06-09 15:36:26 +0400

    Added things 2

--- main.tf
+++ main.tf
@@ -6,14 +6,21 @@ variable "instance_name" {
   type = string
 }
 
+variable "url" {
+  type = string
+}
+
 resource "aws_instance" "in-pub" {
   ami           = "ami-00beae93a2d981137"
   instance_type = "t2.micro"
   user_data     = <<-EOF
     #!/bin/bash
     sudo apt update
+    sudo apt install git
     sudo apt install python3.11
-    python ../calculate.py
+    git clone ${var.url}
+    cd PythonSimple
+    python calculate.py
   EOF
   tags = {
     Name = var.statement == "workflow_dispatch" ? var.instance_name : format("%s-%s", "prod", var.statement)
